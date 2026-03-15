9866163881fd51c49549c788063351e26fd32f14
Zaden Ruggiero-Boune <zaden.ruggieroboune@gmail.com>
2026-03-14 23:48:27 -0400

    Only set xdgdesktopportal platform theme if a portal is available

--- Data/linux-env.sh
+++ Data/linux-env.sh
@@ -4,6 +4,12 @@
 # Add /usr/lib/ to LD_LIBRARY_PATH cause Ubuntu is dumb
 export LD_LIBRARY_PATH="/usr/lib/:$LD_LIBRARY_PATH"
 
+if dbus-send --session --print-reply --dest=org.freedesktop.portal.Desktop /org/freedesktop/portal/Desktop org.freedesktop.DBus.Peer.Ping > /dev/null 2>&1; then
+    # If we have a portal avilable, use the xdgdesktopportal
+    # platform theme to get native dialogs
+    export QT_QPA_PLATFORMTHEME=xdgdesktopportal
+fi
+
 if [[ $(env | grep -i wayland) ]]; then
     # wxWidgets 3.14 is GTK3, which seemingly has an issue or two when
     # running under Wayland. Explicitly setting this for Slippi avoids
